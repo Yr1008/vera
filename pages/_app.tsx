85585aac86eafc582fef2a3fa74042e4dc04238b
Yr1008 <101627882+Yr1008@users.noreply.github.com>
2025-08-08 12:24:47 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,6 @@
-import "../globals.css"; import type { AppProps } from "next/app"; export default function App({ Component, pageProps }: AppProps) { return <Component {...pageProps} />; }
+import "../globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
